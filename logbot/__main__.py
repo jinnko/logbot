4586b9620c48000d47b308e423b8bc1a0b9daa1f
Jinn Koriech <jinn@ixydo.com>
2014-11-09 00:54:05 +0000

    Add support for external config file.
    Attempt to load a config file, with increasing specificity int he following order:
    - /etc/logbot.ini
    - ~/.logbot/config
    - command line args
    
    This can be used to pass config to the bot as well as to override the listening address and port for the httpd service.
    
    Add .gitignore for virtualenv files.

--- logbot/__main__.py
+++ logbot/__main__.py
@@ -5,6 +5,62 @@ from pytz import timezone, UnknownTimeZoneError
 
 from getpass import getuser, getpass
 
+from os.path import exists, expanduser, join
+import ConfigParser
+
+class LogbotConfig(object):
+
+    def __init__(self):
+        self.default_options = {
+            'logbot': {
+                'user': None,
+                'passwd': None,
+                'host': 'localhost',
+                'port': '5222',
+                'tls': 'True',
+                'timezone': None,
+                'rooms': ''
+                },
+            'httpd': {
+                'listen_address': '0.0.0.0',
+                'listen_port': '5000'
+            },
+        }
+        self.opts = {}
+        self.opts.update(self.default_options['logbot'])
+        self.opts.update(self.default_options['httpd'])
+
+        self.config = ConfigParser.SafeConfigParser(self.opts, allow_no_value=True)
+
+        sys_cfg = '/etc/logbot.ini'
+        usr_cfg = join(expanduser('~/.logbot'), 'config')
+        if exists(sys_cfg) or exists(usr_cfg):
+            self.config.read([sys_cfg, usr_cfg])
+
+    def get(self, section, item):
+        try:
+            return self.config.get(section, item)
+        except ConfigParser.NoSectionError as e:
+            if item in self.default_options[section]:
+                return self.default_options[section][item]
+            else:
+                return None
+
+    def getboolean(self, section, item):
+        try:
+            return self.config.getboolean(section, item)
+        except ConfigParser.NoSectionError as e:
+            if item in self.default_options[section]:
+                if self.default_options[section][item] in ['True', 'true', 1, 0, 'T', 't']:
+                    return True
+                else:
+                    return False
+            else:
+                raise Exception
+
+    def items(self, section):
+        return self.config.items(section)
+
 
 def main(argv=None):
     import sys
@@ -12,20 +68,23 @@ def main(argv=None):
 
     argv = argv or sys.argv
 
+    config = LogbotConfig()
+
     parser = ArgumentParser(description='Jabber Log Bot')
-    parser.add_argument('--user', help='jabber user', default=None)
-    parser.add_argument('--passwd', help='jabber password', default=None)
-    parser.add_argument('--host', help='jabber host', default='localhost')
-    parser.add_argument('--port', help='jabber port', default=5222, type=int)
+    parser.add_argument('--user', help='jabber user', default=config.get('logbot', 'user'))
+    parser.add_argument('--passwd', help='jabber password', default=config.get('logbot', 'passwd'))
+    parser.add_argument('--host', help='jabber host', default=config.get('logbot', 'host'))
+    parser.add_argument('--port', help='jabber port', default=config.get('logbot', 'port'), type=int)
     parser.add_argument('--no-tls', help='do not use tls', dest='use_tls',
-                        action='store_false', default=True)
-    parser.add_argument('rooms', help='rooms to log', nargs='+',
-                        metavar='room')
+                        action='store_false', default=config.getboolean('logbot', 'tls'))
     parser.add_argument('--version', action='version',
                         version='logbot {}'.format(__version__))
-    parser.add_argument('--timezone', help='time zone', default=None)
+    parser.add_argument('--timezone', help='time zone', default=config.get('logbot', 'timezone'))
+    parser.add_argument('rooms', help='rooms to log', nargs='+',
+                        metavar='room')
 
-    args = parser.parse_args(argv[1:])
+    argv.extend(config.get('logbot', 'rooms').split())
+    args = parser.parse_args(argv[1:].append(config.get('logbot', 'rooms')))
 
     if args.timezone:
         try:
@@ -44,7 +103,8 @@ def main(argv=None):
     register_listener(log.log)
     register_listener(search.index)
 
-    run_thread(httpd.run)
+    run_thread(httpd.run, [config.items('httpd')])
+
     bot.run(
         args.host,
         args.port,
